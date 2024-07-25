934102a1eb8df6854e47d7f0e9fc09cbae1b518d
alexander gubernikoff <agubernikoff@gmail.com>
2024-07-24 23:29:28 -0400

    bag tweaks due to hard coded width

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -385,6 +385,7 @@ function HeaderCtas({isLoggedIn, cart}) {
     window.location.hash = 'x';
   };
   useEffect(() => {
+    setActiveDropdown('bag');
     if (loc.hash.includes('#bag')) handleMouseEnter('bag');
   }, [loc.hash]);
 
